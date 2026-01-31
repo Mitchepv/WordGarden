26aa4655de0b002a3cb1ee32f03ade8f932f5c3d
Mitchepv <143571339+Mitchepv@users.noreply.github.com>
2026-01-31 17:44:52 -0500

    Initial Commit

---
+++ WordGarden/WordGardenApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordGardenApp.swift
+//  WordGarden
+//
+//  Created by Nia Mitchell on 1/31/26.
+//
+
+import SwiftUI
+
+@main
+struct WordGardenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
